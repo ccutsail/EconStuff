f2b75e8fe032f47d6dda2140a9168c9bb5bde314
ccutsail <ccutsale@udel.edu>
2018-02-10 18:58:35 -0500

    seperated map and started back on gini for PA

--- Aiyagari/gini_ex.py
+++ Aiyagari/gini_ex.py
@@ -5,71 +5,17 @@
 import json
 import urllib
 import pandas as pd
-import numpy as np
 import gini as g
-from mpl_toolkits.basemap import Basemap
-import someoneelsesmaps as sem
-import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
 
-state_topo = r'us_states.topo.json'
-county_topo = r'us_counties.topo.json'
 with urllib.request.urlopen("https://api.census.gov/data/2016/acs/acsse?get=STATE,K200001_001E,K201901_001E&for=county:*&in=state:*") as url:
     DAT = json.load(url)
+
 
 pdData = pd.DataFrame(DAT)
 pdData.columns = pdData.iloc[0]
 pdData.reindex(pdData.index.drop(0))
-pdData["FIPS"] = pdData["STATE"] + pdData["county"]
-pdData = pdData[~pdData.astype(str).eq('None').any(1)]
 pdData["K200001_001E"] = pd.to_numeric(pdData["K200001_001E"],errors='coerce')
-pdData = pdData.dropna(axis=0, how='any')
-# generate the ginicoefficient -- this isn't working yet
-#dull = g.ginicoefficient(TAB[:, [0]], TAB[:, [1]])
-#incvals = map(lambda a:int(pdDat[a]),pdDat)
-pdData.to_csv(r"C:\\")
-county_color_dict = {}
-for index, row in pdData.iterrows():
-    if row["K200001_001E"] is not None and index > 0:
-        color = sem.calculate_color(int(row["K200001_001E"]),min(pdData["K200001_001E"]),max(pdData["K200001_001E"]),10)
-        county_color_dict[row["FIPS"]] = color
+pdData["K201901_001E"] = pd.to_numeric(pdData["K201901_001E"],errors='coerce')
+pdData["INCPERCAPITA"] = pdData["K201901_001E"]/pdData["K200001_001E"]
 
-patches = dict(map(lambda color:(color,[]),county_color_dict.values()))
-
-# Set the lower left and upper right limits of the bounding box:
-lllon = -119
-urlon = -64
-lllat = 22.0
-urlat = 50.5
-# and calculate a centerpoint, needed for the projection:
-centerlon = float(lllon + urlon) / 2.0
-centerlat = float(lllat + urlat) / 2.0
-
-m = sem.draw_us_map()
-
-###### This is where the current problem is
-###### map doesn't have county info or counties stored
-###### uses example shapefile -- need to review shapefile to figure out if that
-###### is the problem
-
-for info, shape in zip(m.counties_info, m.counties):
-    try:
-        color=county_color_dict[info['GEOID'].zfill(5)]
-        patches[color].append(Polygon(np.array(shape), True) )
-    except Exception as e:
-        continue
-plt.title('US Counties')
-# Get rid of some of the extraneous whitespace matplotlib loves to use.
-plt.tight_layout(pad=0, w_pad=0, h_pad=0)
-plt.show()
-'''
-# This code imports as a numpy array. After a ton of debugging, it seems like it'd be easier to do
-# what I'm trying to do with a pandas dataframe. If the pandas dataframe doesn't work out, I'll
-# switch back to a numpy array bc this code is functional 
-TAB = np.array([0, 0])
-for i in range(1, len(DAT)):
-    if(DAT[i][2] is not None and DAT[i][1] is not None):
-        appender = [int(DAT[i][2]), int(DAT[i][1])]
-        TAB = np.vstack([TAB, appender])
-'''
+print(g.ginicoefficient(pdData["K200001_001E"],pdData["INCPERCAPITA"]))
---
+++ Aiyagari/incPerCapMap.py
@@ -0,0 +1,83 @@
+''' This is a map of income by population for the state of Pennsylvania
+    It functions, but is missing some data.
+    Shapefile from here: https://catalog.data.gov/dataset/tiger-line-shapefile-2013-state-pennsylvania-current-county-subdivision-state-based
+    Credit to here: http://www.zimlon.com/b/using-matplotlib-basemap-to-visualize-home-insurance-by-county-cm615
+    and here: http://shallowsky.com/blog/programming/plotting-election-data-basemap.html
+    some of the code from those sites is used verbatim or heavily adapted
+'''
+import json
+import urllib
+import pandas as pd
+import numpy as np
+import gini as g
+from mpl_toolkits.basemap import Basemap
+import someoneelsesmaps as sem
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+import shapefile
+import math
+
+def calculate_color(countyval, minval, maxval, colorlevels):
+    from math import ceil
+    level = ceil((countyval-minval)*colorlevels/(maxval-minval))
+    color = 255-ceil(level*255/colorlevels)
+    return color
+
+with urllib.request.urlopen("https://api.census.gov/data/2016/acs/acsse?get=STATE,K200001_001E,K201901_001E&for=county:*&in=state:*") as url:
+    DAT = json.load(url)
+
+pdData = pd.DataFrame(DAT)
+pdData.columns = pdData.iloc[0]
+pdData.reindex(pdData.index.drop(0))
+pdData["FIPS"] = "00" + pdData["county"]
+pdData = pdData[~pdData.astype(str).eq('None').any(1)]
+pdData["K200001_001E"] = pd.to_numeric(pdData["K200001_001E"],errors='coerce')
+pdData["K201901_001E"] = pd.to_numeric(pdData["K201901_001E"],errors='coerce')
+pdData["INCPERCAPITA"] = pdData["K201901_001E"]/pdData["K200001_001E"]
+pdData = pdData.dropna(axis=0, how='any')
+
+county_color_dict = {}
+for index, row in pdData.iterrows():
+    if row["INCPERCAPITA"] is not None and index > 0 and row["STATE"] == "42":
+        color = calculate_color(int(row["INCPERCAPITA"]),min(pdData["INCPERCAPITA"]),max(pdData["INCPERCAPITA"]),100)
+        county_color_dict[row["FIPS"]] = color
+        print(row["FIPS"])
+
+patches = dict(map(lambda color:(color,[]),county_color_dict.values()))
+
+ax = plt.subplot()
+
+lllon = -81
+urlon = -75
+lllat = 39
+urlat = 43
+
+centerlon = float(lllon + urlon) / 2.0
+centerlat = float(lllat + urlat) / 2.0
+
+m = Basemap(resolution='i',  
+            llcrnrlon = lllon, urcrnrlon = urlon,
+            lon_0 = centerlon,
+            llcrnrlat = lllat, urcrnrlat = urlat,
+            lat_0 = centerlat,
+            projection='tmerc')
+
+
+m.readshapefile('tl_2013_42_cousub','counties',drawbounds=True)
+
+
+for info, shape in zip(m.counties_info, m.counties):
+    try:
+        color=county_color_dict[info['COUNTYFP'].zfill(5)]
+        patches[color].append(Polygon(np.array(shape), True) )
+    except Exception as e:
+        continue
+for color in patches:
+    htmlcolor = '#%02x%02x%02x' % (math.ceil((255-color)/2), 100, math.ceil(color/2))
+    ax.add_collection(PatchCollection(patches[color],facecolor=htmlcolor , edgecolor='k', linewidths=1.0, zorder=2))
+
+plt.title('Income Per Capita in Pennsylvania')
+plt.tight_layout(pad=0, w_pad=0, h_pad=0)
+plt.show()     
+
