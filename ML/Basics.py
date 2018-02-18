acf858c829585c684a8c5574c96c449f1e5a05b4
ccutsail <ccutsale@udel.edu>
2018-02-18 09:31:28 -0500

    figure out the nba_py json to df process

--- ML/Basics.py
+++ ML/Basics.py
@@ -1,9 +1,20 @@
-from sklearn import datasets
 import pandas as pd
-from nba_py import team
+import nba_py
+from nba_py import player
+import altair as plt
+import pprint
 
-clips = team.TeamLineups(1610612746)
+#clips = team.TeamSummary(1610612746)
+#ptDiff = clips.season_ranks()["PTS_PG"]-clips.season_ranks()["OPP_PTS_PG"]
+# blake = team.TeamVsPlayer(1610612746,201933)
+# pprint(blake.on_off_court()["W"]/blake.on_off_court()["GP"])
 
-isBlakeGood = team.TeamVsPlayer(1610612746,201933)
+blakeCareer = nba_py.player.PlayerCareer(201933)
 
-print(isBlakeGood.shot_distance_off_court().head(10),isBlakeGood.shot_distance_on_court().head(10))
+careerAvg = blakeCareer.regular_season_career_totals()
+
+careerLog = nba_py.player.PlayerGameLogs(201933)
+
+df = pd.read_json(careerLog.json)
+
+list(df.columns)
