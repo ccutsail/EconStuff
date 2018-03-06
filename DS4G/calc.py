c72548d6385dea7a74c2521fad2e10e88dd85540
ccutsail <ccutsale@udel.edu>
2018-03-05 21:00:14 -0500

    sigmoid + kiva

---
+++ DS4G/calc.py
@@ -0,0 +1,8 @@
+def gradDescent(theta, X, y, *kwargs):
+    import numpy as np
+    (np.dot(X,theta)-y)
+
+def sigmoid(x):
+    import numpy as np
+    return 1/(1+np.exp(np.multiply(-1,[1,1,1])))
+    
